75bafd008ac9264292b57348f730387573d16804
Ronald <ronald.kooij@nl.thalesgroup.com>
2020-09-28 15:02:00 +0200

    Setup for Unity Package Manager

---
+++ Packages/AssetGuide/Editor/FolderEditor.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System;
+
+[CustomEditor(typeof(DefaultAsset))]
+public class FolderEditor : Editor
+{
+    private string _path;
+    private AssetImporter _importer;
+    private FolderInfo _info;
+
+    protected override void OnHeaderGUI()
+    {
+        if (ValidateTarget())
+        {
+            FolderGUI();
+        }
+        else
+        {
+            base.OnHeaderGUI();
+        }
+    }
+
+    //Called when a folder is selected, get our info here
+    private void OnEnable()
+    {
+        if (!ValidateTarget())
+            return;
+
+        try
+        {
+            _info = JsonUtility.FromJson<FolderInfo>(_importer.userData);
+        }
+        catch (NullReferenceException)
+        {
+            //Debug.Log("Userdata either doesnt exist, or is corrupted");
+        }
+    }
+
+    //Called when a folder is deselected, save our info here
+    private void OnDisable()
+    {
+        if (!ValidateTarget())
+            return;
+
+        _importer.userData = JsonUtility.ToJson(_info);
+    }
+
+    //Visualise the info here
+    private void FolderGUI()
+    {
+        _info.locked = GUILayout.Toggle(_info.locked, "Locked");
+        _info.description = GUILayout.TextArea(_info.description);
+
+        if (GUILayout.Button("scream"))
+        {
+            Debug.Log(_info.description);
+        }
+    }
+
+    [Serializable]
+    private struct FolderInfo
+    {
+        public string description;
+        public bool locked;
+    }
+
+    /// <summary>
+    /// Check if the target is viable for the special folder stuff,
+    /// So make sure it is a folder, and not in the packages area
+    /// </summary>
+    /// <returns></returns>
+    private bool ValidateTarget()
+    {
+        _path = AssetDatabase.GetAssetPath(target);
+        _importer = AssetImporter.GetAtPath(_path);
+
+        if (!Directory.Exists(_path) || _path.StartsWith("Packages") || _importer == null)
+            return false;
+        else
+            return true;
+    }
+}
---
+++ Packages/AssetGuide/Editor/FolderLocker.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.Experimental.AssetImporters;
+
+public class PermissionChecker : ScriptedImporter
+{
+    public override void OnImportAsset(AssetImportContext ctx)
+    {
+
+        /*
+        string fileContent = File.ReadAllText(ctx.assetPath);
+        var booleanObj = ObjectFactory.CreateInstance<BooleanClass>();
+        if (!bool.TryParse(fileContent, out booleanObj.boolean))
+        {
+            booleanObj.boolean = false;
+        }
+        ctx.AddObjectToAsset("main", booleanObj);
+        ctx.SetMainObject(booleanObj);
+        Debug.Log("Imported Boolean value: " + booleanObj.boolean);
+        */
+
+        
+    }
+
+
+    /*
+    PermissionChecker : UnityEditor.AssetModificationProcessor
+        private static AssetMoveResult OnWillMoveAsset(string sourcePath, string destinationPath)
+        {
+            Debug.Log("Source path: " + sourcePath + ". Destination path: " + destinationPath + ".");
+            AssetMoveResult assetMoveResult = AssetMoveResult.DidNotMove;
+
+            // Perform operations on the asset and set the value of 'assetMoveResult' accordingly.
+
+            return assetMoveResult;
+        }
+    */
+
+    /*
+    static void OnPostprocessAllAssets(string[] importedAssets, string[] deletedAssets, string[] movedAssets, string[] movedFromAssetPaths)
+    {
+        foreach (string str in importedAssets)
+        {
+            Debug.Log("Reimported Asset: " + str);
+        }
+        foreach (string str in deletedAssets)
+        {
+            Debug.Log("Deleted Asset: " + str);
+        }
+
+        for (int i = 0; i < movedAssets.Length; i++)
+        {
+            Debug.Log("Moved Asset: " + movedAssets[i] + " from: " + movedFromAssetPaths[i]);
+        }
+    }*/
+}
